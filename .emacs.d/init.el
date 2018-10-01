c786faff076e8950929f04537b924343cbba98e6
Boeglin, Thomas (Agoda) <thomas.boeglin@agoda.com>
2018-10-01 17:44:58 +0700

    Change smartparens conf a bit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -313,22 +313,20 @@
   sp-restrict-to-pairs-interactive
   sp-local-pair
 
-  :init
-  (setq sp-interactive-dwim t)
-
   :config
   (require 'smartparens-config)
+  (require 'smartparens-scala)
   (sp-use-smartparens-bindings)
-  (sp-pair "(" ")" :wrap "C-(")
-  (sp-pair "[" "]" :wrap "s-[") ;; C-[ sends ESC
-  (sp-pair "{" "}" :wrap "C-{")
+  ;; (sp-pair "(" ")" :wrap "C-(")
+  ;; (sp-pair "[" "]" :wrap "s-[") ;; C-[ sends ESC
+  ;; (sp-pair "{" "}" :wrap "C-{")
   ;; (sp-local-pair 'scala-mode "(" nil :post-handlers '(("||\n[i]" "RET")))
   ;; (sp-local-pair 'scala-mode "{" nil :post-handlers '(("||\n[i]" "RET") ("| " "SPC")))
   (bind-key "s-{" 'sp-rewrap-sexp smartparens-mode-map)
 
   ;; WORKAROUND https://github.com/Fuco1/smartparens/issues/543
-  (bind-key "C-<left>" nil smartparens-mode-map)
-  (bind-key "C-<right>" nil smartparens-mode-map)
+  ;; (bind-key "C-<left>" nil smartparens-mode-map)
+  ;; (bind-key "C-<right>" nil smartparens-mode-map)
 
   (bind-key "s-<delete>" 'sp-kill-sexp smartparens-mode-map)
   (bind-key "s-<backspace>" 'sp-backward-kill-sexp smartparens-mode-map))
